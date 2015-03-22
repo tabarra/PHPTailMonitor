11bbb8f8160c142c7f7ef53f6229372ab2a08065
matheus lima <whoismath@gmail.com>
2015-03-21 21:55:06 -0300

    Update index.php
    mudando o layout

--- index.php
+++ index.php
@@ -11,16 +11,30 @@
     <script src="res/jquery-timing.min.js"></script>
 </head>
 <body>
-    <div class="menu">
-        <h3>PHPTailMonitor</h3>
-        <button id="AutoScroll">Disable AutoScroll</button>
-        <button id="disable">Stop</button>
-        <button id="Clear">Clear Log</button>
-        <a href="login.php?logout">Logout</a>
-    </div>
-
-	<div id="tailLog"></div>
+	<div id="navbar-container">
+            <nav id="navbar-section">
+                <ul id="navbar-left-list" class="navbar-list">
+                    <li class="navbar-item navbar-title">
+                        <h3>PHPTailMonitor</h3>
+                    </li>
+                    <li class="navbar-item">
+                        <a id="AutoScroll" class="nav-button nav-button-blue" href="#">Disable AutoScroll</a>
+                    </li>
+                    <li class="navbar-item">
+                        <a id="disable" class="nav-button nav-button-blue" href="#">Stop</a>
+                    </li>
+                    <li class="navbar-item">
+                        <a id="Clear" class="nav-button nav-button-blue" href="#">Clear Log</a>
+                    </li>
+                </ul>
+                <ul id="navbar-right-list" class="navbar-list">
+                    <li class="navbar-item">
+                        <a class="nav-button nav-button-red" href="login.php?logout">Logout</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div id="container-content"></div>
 	<div id="scrollLock"></div>
-    <script src="res/functions.js"></script>
-</body>
+    </body>
 </html>
